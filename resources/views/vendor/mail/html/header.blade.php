672cfb81dc72dfe26d712672494f78c050b6d012
santysos <santysos@hotmail.com>
2024-09-27 16:13:51 -0500

    Correcion logo en header del email enviado

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -5,7 +5,7 @@
 @if (trim($slot) === 'Laravel')
 <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
 @else
-<img src="https://helmet.ergomas.ec/logo-helmet-2.webp" class="logo" alt="Helmet Logo">
+<img src="https://helmet.ergomas.ec/public/images/logo-h.png" class="logo" alt="Helmet Logo">
 @endif
 </a>
 </td>
